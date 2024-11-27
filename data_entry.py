d985b690634bef51a902a0520c7c957942e70698
Trust-Worthy <jondeveloper0@gmail.com>
2024-11-27 08:31:40 -0500

    Finished the main class functions for the finance tracker.

---
+++ data_entry.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+
+def get_date(prompt:str, allow_default=False):
+    raise NotImplementedError
+
+def get_amount():
+    raise NotImplementedError
+
+def get_category():
+    raise NotImplementedError
+
+def description():
+    raise NotImplementedError
--- main.py
+++ main.py
@@ -4,11 +4,32 @@ from datetime import datetime
 
 
 class CSV:
-    CSV_FILE = 'finance_data.csv'
+    CSV_FILE: str = 'finance_data.csv'
+    COLUMNS: list = ["date","amount","category","description"]
 
     @classmethod
-    def initialize_csv(cls):
+    def initialize_csv(cls)->None:
         try:
             pd.read_csv(cls.CSV_FILE)
         except FileNotFoundError:
-            df = pd.DataFrame(columns=["date","amount","category","description"])
+            df = pd.DataFrame(columns=cls.COLUMNS)
+            df.to_csv(cls.CSV_FILE,index=False)
+
+    @classmethod
+    def add_entry(cls,date:str,amount:int,category:str,description:str)->None:
+        new_entry: dict = {
+            "date": date,
+            "amount": amount,
+            "category": category,
+            "description":description
+        }
+        with open(cls.CSV_FILE,"a",newline="") as csvfile:
+            writer = csv.DictWriter(csvfile,fieldnames=cls.COLUMNS)
+            writer.writerow(new_entry)
+        print("Entry added successfully")
+
+    
+
+
+CSV.initialize_csv()
+CSV.add_entry("20-07-2024",125.64,"income","Salary")

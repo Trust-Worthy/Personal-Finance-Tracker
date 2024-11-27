1721144705cfdc4c7e9a4ee60b4ce44be96249fb
Trust-Worthy <jondeveloper0@gmail.com>
2024-11-27 10:16:20 -0500

    Testing the main function.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from data_entry import get_amount,get_category,get_date,get_description
 class CSV:
     CSV_FILE: Union[str,Path] = 'finance_data.csv'
     COLUMNS: list[str] = ["date","amount","category","description"]
-
+    FORMAT: str = "%d-%m-%Y"
     @classmethod
     def initialize_csv(cls)->None:
         try:
@@ -31,6 +31,38 @@ class CSV:
             writer.writerow(new_entry)
         print("Entry added successfully")
 
+    @classmethod
+    def get_transactions(cls,*,start_date_arg:str,end_date_arg:str)->pd.DataFrame:
+        df:pd.DataFrame = pd.read_csv(cls.CSV_FILE)
+
+        df["date"] = pd.to_datetime(df["date"],format=CSV.FORMAT)
+
+        start_date:datetime = datetime.strptime(start_date_arg,CSV.FORMAT)
+        end_date: datetime = datetime.strptime(end_date_arg,CSV.FORMAT)
+
+        mask = (df["date"] >= start_date) & (df["date"] <= end_date)
+        filtered_df:pd.DataFrame = df.loc[mask] # returning
+
+
+        if filtered_df.empty:
+            print("No transactions found in given date range")
+        else:
+            print(
+                f"Transactions from ({start_date.strftime(CSV.FORMAT)}) to ({end_date.strftime(CSV.FORMAT)}) ")
+            print(
+                filtered_df.to_string(
+                    index=False,formatters={"date":lambda x:x.strftime(CSV.FORMAT)}
+                )
+            )
+        total_income:float = filtered_df[filtered_df["category"] == "Income"]["amount"].sum()
+        total_expense:float = filtered_df[filtered_df["category"] == "Expense"]["amount"].sum()
+        print("\nSummary:")
+        print(f"Total Income: ${total_income:.2f}")
+        print(f"Total Expenses: ${total_expense:.2f}")
+        print(f"Net Savings ${(total_income - total_expense):.2f}")
+
+
+        return filtered_df # this will be used to plot the expenses
 def add()->None:
     CSV.initialize_csv()
     date: str = get_date(
@@ -45,5 +77,32 @@ def add()->None:
 
     
 
+#CSV.get_transactions(start_date_arg="01-01-2023",end_date_arg="30-07-2025")
 
-add()
+
+def main()->None:
+
+    while True:
+        print("\n1. Add a new transaction")
+        print("2. View Transactions and summary within a date range")
+        print("3. Exit")
+
+        choice: str = input("Enter your choice (1-3): ")
+
+        if choice == "1":
+            add()
+        elif choice == "2":
+            start_date:str = get_date(prompt="Enter the start date (dd-mm-yyyy)")
+            end_date:str = get_date(prompt="Enter the end date (dd-mm-yyyy)")
+            df_result:pd.DataFrame = CSV.get_transactions(start_date_arg=start_date,end_date_arg=end_date)
+
+        elif choice == "3":
+            print("Exiting program ...")
+            break
+        else:
+            print("Invalid choise. Enter 1, 2, or 3.")
+
+if __name__ == "__main__":
+    main()
+            
+        

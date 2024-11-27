e1c9c13cf37df5689212de55561a9a004b7c6d6b
Trust-Worthy <jondeveloper0@gmail.com>
2024-11-27 07:54:11 -0500

    Forgot to rename the method.

--- oop_refresh.py
+++ oop_refresh.py
@@ -14,7 +14,7 @@ class Microwave:
             self.turned_on = True
             print(f"Microwave ({self.brand}) is now turned on")
     
-    def turn_on(self)->None:
+    def turn_off(self)->None:
         if self.turned_on:
             self.turned_on = False
             print(f"Microwave ({self.brand}) is now turned off")
@@ -29,4 +29,7 @@ class Microwave:
 
 
 m1: Microwave = Microwave(brand="yeep",power_rating="A+")
+
+m1.turn_on()
+m1.turn_on()
 

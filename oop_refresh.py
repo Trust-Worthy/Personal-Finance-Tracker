132e488ddcc5523bbeb0f6ef887ba9440ab838da
Trust-Worthy <jondeveloper0@gmail.com>
2024-11-27 07:52:19 -0500

    Continuing my oop refresh.

--- oop_refresh.py
+++ oop_refresh.py
@@ -10,6 +10,21 @@ class Microwave:
     def turn_on(self)->None:
         if self.turned_on:
             print(f"Microwave ({self.brand}) is already turned on")
+        else:
+            self.turned_on = True
+            print(f"Microwave ({self.brand}) is now turned on")
+    
+    def turn_on(self)->None:
+        if self.turned_on:
+            self.turned_on = False
+            print(f"Microwave ({self.brand}) is now turned off")
+        else:
+            print(f"Microwave ({self.brand}) is already turned off")
+    def run(self,seconds:int)->None:
+        if self.turned_on:
+            print(f'Running ({self.brand}) for {seconds} seconds')
+        else:
+            print(f'A mystical forcer whispers: "Turn on your microwave first..."')
 
 
 
